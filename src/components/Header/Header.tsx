64669e1c0bbe7639020313d07a8dceb59835dcb8
Yelyzaveta Smetana <yelyzaveta.smetana@hneu.net>
2024-05-05 20:43:51 +0300

    some settings

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,9 @@
 import {NavLink, useLocation, useNavigate} from "react-router-dom";
 import css from './Header.module.css'
-import {FormEvent, useEffect, useState} from "react";
+import React, {FormEvent, useEffect, useState} from "react";
 import {supabase} from "../../constans/dT";
+import Logo from './Logo.png';
+
 import {useAuthContext} from "../../hoc/AuthContext";
 const Header = () => {
     const navigate = useNavigate();
@@ -32,33 +34,50 @@ const Header = () => {
     return (
         <div className={css.Header}>
             {isAdmin && (
-                <div className={css.navigator}>
-                    <NavLink to={'admin'}>Хто зробив Запис на донорство</NavLink>
-                    <button onClick={handleLogout}>Вийти</button>
+                <div className={css.lodo}>
+                    <img alt={'LogoDonor'} className={css.ImageLOGO}
+                         src={Logo}/>
+                    <div className={css.navigator}>
+                        <NavLink to={'admin'}>Хто зробив Запис на донорство</NavLink>
+                        <button onClick={handleLogout}>Вийти</button>
+                    </div>
+
+
                 </div>
             )}
 
             {isUser && (
-                <div className={css.navigator2}>
-                    <NavLink to={`/registration-donation/${localStorage.getItem('id_registration')}`}>Зробити
-                        запис на донорство крові</NavLink>
-                    <div className={css.home}>
-                        <NavLink to={`/donor/${localStorage.getItem('id_registration')}`}>
-                            <img alt={'account'}
-                                 src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAu0lEQVR4nO2U0QnCMBRFzwQdQOufpcPoKrqG6CgSOoMWl1DoCIIt6G+k8AoaNKnk9UP0wv1JyjmkLy38SiaAARppAeSa8DNgnbZrqYbAvIB33WoIGo+g1hDYQAc9wUVDUHgE7Xyik3tuUYZSUrkxtdRowr83Y2ADVEAJJA97iaxV8szoU/gSuDlD3QFTefd7Z+8KLPrC1z0+LvumqxB8HgG30plPcFIQHH2CWLgN/Z8GFxwU4GVgzv/wlDuj6q8tGM8DawAAAABJRU5ErkJggg=="/>
-                        </NavLink>
-                        <button className={css.homebutton} onClick={handleLogout}>Вийти</button>
-                    </div>
+                <div className={css.lodo}>
+                    <img alt={'LogoDonor'} className={css.ImageLOGO}
+                         src={Logo}/>
+                    <div className={css.navigator2}>
+                        <NavLink to={`/registration-donation/${localStorage.getItem('id_registration')}`}>Зробити
+                            запис на донорство крові</NavLink>
+                        <div className={css.home}>
+                            <NavLink to={`/donor/${localStorage.getItem('id_registration')}`}>
+                                <img alt={'account'}
+                                     src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAu0lEQVR4nO2U0QnCMBRFzwQdQOufpcPoKrqG6CgSOoMWl1DoCIIt6G+k8AoaNKnk9UP0wv1JyjmkLy38SiaAARppAeSa8DNgnbZrqYbAvIB33WoIGo+g1hDYQAc9wUVDUHgE7Xyik3tuUYZSUrkxtdRowr83Y2ADVEAJJA97iaxV8szoU/gSuDlD3QFTefd7Z+8KLPrC1z0+LvumqxB8HgG30plPcFIQHH2CWLgN/Z8GFxwU4GVgzv/wlDuj6q8tGM8DawAAAABJRU5ErkJggg=="/>
+                            </NavLink>
+                            <button className={css.homebutton} onClick={handleLogout}>Вийти</button>
+                        </div>
 
+                    </div>
                 </div>
+
+
             )}
 
             {!isAdmin && !isUser && (
-                <div className={css.navigator}>
-                    <NavLink to={'informations'}>Загальна інформація</NavLink>
-                    <NavLink to={'confirmation'}>Запис на донорство</NavLink>
-                    <NavLink to={'login'}>Увійти</NavLink>
-                    <NavLink to={'register'}>Реєстрація</NavLink>
+                <div className={css.lodo}>
+                    <img alt={'LogoDonor'} className={css.ImageLOGO}
+                         src={Logo}/>
+                    <div className={css.navigator}>
+                        <NavLink to={'informations'}>Загальна інформація</NavLink>
+                        <NavLink to={'confirmation'}>Запис на донорство</NavLink>
+                        <NavLink to={'login'}>Увійти</NavLink>
+                        <NavLink to={'register'}>Реєстрація</NavLink>
+                    </div>
+
                 </div>
             )}
         </div>
